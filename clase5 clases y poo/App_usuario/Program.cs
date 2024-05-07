d589c9b7f3e382e8e789ac2c3d195e30484d7d24
M1guelpereira0797 <110005364+M1guelpereira0797@users.noreply.github.com>
2024-05-07 19:45:46 -0400

    clase usuario
    me falta aprender como se hace desde la consola

---
+++ clase5 clases y poo/App_usuario/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace App_usuario
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            bool mayor;
+            string nuevodireccion;
+            bool gmail;
+
+            Usuario usuario1 = new Usuario(" pedro", " apodo", 222, "pedroapodo07", 3298, 51254, "CRMALO", "pedroapodo@gmail.com");
+            ;
+            Usuario usuario2 = new Usuario(" juan ", " apodo", 22, "juanapodo07",989898, 545454,   "condorito", "juanapodo@gmail.com");
+
+
+            mayor = usuario1.Esmayordeedad();
+            Console.WriteLine("Usuario 1 tiene correo gmail " + mayor   );
+            mayor = usuario2.Esmayordeedad();
+            Console.WriteLine("Usuario 2 tiene correo gmail " + mayor);
+            nuevodireccion = usuario1.Cambiardireccion("");
+            Console.WriteLine("Usuario 1 tiene correo gmail " + nuevodireccion);
+            nuevodireccion = usuario2.Cambiardireccion("1");
+            Console.WriteLine("Usuario 2 tiene correo gmail " + nuevodireccion);
+            gmail = usuario1.gmaile();
+            Console.WriteLine("Usuario 1 tiene correo gmail " + gmail);
+            gmail = usuario2.gmaile();
+            Console.WriteLine("Usuario 2 tiene correo gmail " + gmail);
+
+
+
+
+
+
+        }
+    }
+}
---
+++ clase5 clases y poo/App_usuario/Usuario.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_usuario
+{
+    public class Usuario
+    {
+        private string _nombre;
+        private string _apellido;
+        private int _edad;
+        private string _id;
+        private int _contrasena;
+        private int _RUT;
+        private string _domicilio;
+        private string _mail;
+
+        public Usuario(string nombre, string apellido, int edad, string id, int contrasena, int rut, string domicilio, string mail)
+        {
+            
+            this._nombre = nombre;
+            this._apellido = apellido;
+            this._edad = edad;
+            this._id = id;
+            this._contrasena = contrasena;
+            this._RUT = rut;
+            this._domicilio = domicilio;
+            this._mail = mail;
+
+        }
+        public string mail
+        {
+            get
+            {
+                return this._mail;
+            }
+            set
+            {
+                this._mail = value;
+            }
+
+         }
+        public bool Esmayordeedad()
+        {
+            return this._edad >= 18;
+        }
+        public string Cambiardireccion(string nuevodireccion)
+        {
+            this._domicilio = nuevodireccion;
+
+            return this._domicilio;
+        }
+        public bool gmaile()
+        {
+            if(this._mail.EndsWith("gmail.com"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+
+    }
+    
+
+}
