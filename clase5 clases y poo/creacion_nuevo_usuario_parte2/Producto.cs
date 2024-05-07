5eea51926bc6f1c70b58724290f5ea34778b7a2f
M1guelpereira0797 <110005364+M1guelpereira0797@users.noreply.github.com>
2024-05-07 18:36:54 -0400

    get y set
    entendiendo como se utiliza el get y el set

--- clase5 clases y poo/creacion_nuevo_usuario_parte2/Producto.cs
+++ clase5 clases y poo/creacion_nuevo_usuario_parte2/Producto.cs
@@ -6,7 +6,82 @@ using System.Threading.Tasks;
 
 namespace creacion_nuevo_usuario_parte2
 {
-    internal class Producto
+    public class Producto
     {
+        private int _numero;
+        private string _descripcion;
+        private string _categoria;
+        private double _preciodeventa;
+        private double _preciodecompra;
+
+
+        public Producto(int numero,string descripcion, string categoria, double preciodecompra, double preciodeventa)
+        {
+            //ahora => antes 
+
+            this._numero= numero;   //numero = 0;
+            this._descripcion = descripcion;//descripcion =   string.Empty;
+            this._categoria = categoria;//categoria = string.Empty;   
+            this._preciodecompra = preciodecompra;//preciodecompra = 0;
+            this._preciodeventa = preciodeventa; //preciodeventa = 0;  
+            
+         }
+
+        public bool haypreciodeventa()
+        {
+            return this._preciodeventa > 0;  
+        }
+        public int numero 
+        {
+            get
+            {
+                return this._numero;
+            }       
+            set
+            {
+                this._numero = value;
+            }
+        }
+        public string descripcion
+        {
+            get
+            {
+                return this._descripcion;
+            }
+            set
+            {
+                this._descripcion = value;
+            }
+
+
+
+
+        }
+        public string categoria
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(this._categoria))
+                {
+                    return this._categoria;
+                }
+                else
+                {
+                    return "Sin categoria";
+                }
+            }
+            set
+            {
+                this._categoria = value;
+            }
+
+
+
+
+        }
+        public double preciodecompra { get; set; }
+        public double preciodeventa { get; set; }
+
     }
 }
+
--- clase5 clases y poo/creacion_nuevo_usuario_parte2/Program.cs
+++ clase5 clases y poo/creacion_nuevo_usuario_parte2/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace creacion_nuevo_usuario_parte2
+{
+
+
+
+    public class Probarobjetos
+    {
+        static void Main(string[] args)
+        {
+            /* Producto producto = new Producto(01, "panal", "limpieza", 2.9, 0);
+             if(producto.haypreciodeventa())
+             {
+                 Console.WriteLine("Si hay a la venta");
+
+             }
+             else
+             {
+                 Console.WriteLine("No hay producto a la venta");
+             }*/
+            Producto producto1 = new Producto(01 , null, " panales " ,  5 , 6.9 );
+            
+
+            producto1.numero = 02 ;
+            producto1.descripcion = " pañales pequeños " ;
+            producto1.categoria = "" ;
+            producto1.preciodecompra = 5 ;
+            producto1.preciodeventa = 6.9 ;
+
+            if (producto1.haypreciodeventa())
+            {
+                Console.WriteLine(
+                " Codigo: " + producto1.numero + " " +
+                " descripcion: " + producto1.descripcion + " " +
+                " precio de compra: " + producto1.preciodecompra + "" +
+                " prceio de venta: " + producto1.preciodeventa + "" +
+                " \nCategoria: " + producto1.categoria);
+            }
+            else
+            {
+                Console.WriteLine("El prodcuto aun no se sube");
+            }
+            Producto producto2 = new Producto(03, "desodorante", null, 5, 10);
+
+            producto2.numero = 03;
+            producto2.descripcion = "desodo";
+            producto2.preciodecompra = 10;
+
+            if (producto2.haypreciodeventa())
+            {
+                Console.WriteLine(
+               "\nCodigo: " + producto2.numero + " " +
+               "\ndescripcion: " + producto2.descripcion + " " +
+               "\nprecio de compra: " + producto2.preciodecompra + "" +
+               "\nprceio de venta: " + producto2.preciodeventa + "" +
+                "\nCategoria: " + producto2.categoria);
+            }
+            else
+            {
+                Console.WriteLine("El producto2 aun no se a cargado a sistema ");
+            }
+
+
+
+
+
+
+            
+        }
+    }
+
+}
