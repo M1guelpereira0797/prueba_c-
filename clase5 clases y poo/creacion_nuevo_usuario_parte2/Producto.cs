ec48d84c2fdb31cf46d0b76945f93e01ccbbc75e
M1guelpereira0797 <110005364+M1guelpereira0797@users.noreply.github.com>
2024-05-07 16:39:03 -0400

    clase
    creando una clase

---
+++ clase5 clases y poo/creacion_nuevo_usuario_parte2/Producto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace creacion_nuevo_usuario_parte2
+{
+    internal class Producto
+    {
+    }
+}
