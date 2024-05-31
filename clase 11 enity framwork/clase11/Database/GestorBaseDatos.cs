8f0d4fee70c243307b5f4ae51f6164516cf60a22
M1guelpereira0797 <110005364+M1guelpereira0797@users.noreply.github.com>
2024-05-30 20:26:08 -0400

    clase 11
    dañando y reparando la base de datos, igual no sirvio jajajaja

---
+++ clase 11 enity framwork/clase11/Database/GestorBaseDatos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using clase11.Database;
+using clase11.Models;
+
+
+
+namespace clase11.Database
+{
+    public class GestorBaseDatos : DbContext
+    {
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connecxionString = @"Server=localhost\SQLEXPRESS01;Database=SistemaGestion;Trusted_Connection=True;";
+            optionsBuilder.UseSqlServer(connecxionString);
+        }
+
+    }
+
+
+} 
+
---
+++ clase 11 enity framwork/clase11/Models/Producto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+using Microsoft.EntityFrameworkCore;
+using clase11.Database;
+using clase11.Models;
+
+namespace clase11.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public double PrecioCompra { get; set; }
+        public double PrecioVenta { get; set; }
+        public double Stock { get; set; }
+        public double TotalProducto { get; set; }
+        public string Categoria { get; set; }
+
+    }
+}
