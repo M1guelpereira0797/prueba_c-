7e37f8f6c7296a8deacaade8f350a073077ae220
M1guelpereira0797 <110005364+M1guelpereira0797@users.noreply.github.com>
2024-06-20 20:25:50 -0400

    ultima clase
    a por el desafioooo

---
+++ Clase_16/HttpCliente/ConsumoApi.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpCliente
+{
+    public class ConsumoApi
+    {
+        private readonly HttpClient _client;
+        private readonly string _basePath;
+
+        public ConsumoApi(string basePath)
+        {
+            _client = new HttpClient();
+            _basePath = basePath;
+        }
+
+        /******/
+        /******/
+        /******/
+
+        public async Task<List<Usuario>> FiltrarAsync()
+        {
+            List<Usuario>? list = null;
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync(_basePath);
+                if (response.IsSuccessStatusCode)
+                {
+                    list = await response.Content.ReadFromJsonAsync<List<Usuario>>();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error con la API: " + ex.Message);
+            }
+            return list;
+        }
+
+        /******/
+        /******/
+        /******/
+
+        public async Task<bool> ModificarCliente(Usuario cliente)
+        {
+            HttpResponseMessage response = await _client.PutAsJsonAsync(_basePath, cliente);
+            response.EnsureSuccessStatusCode();
+
+            return response.IsSuccessStatusCode;
+
+        }
+
+        public async Task<bool> AltaCliente(Usuario cliente)
+        {
+            HttpResponseMessage response = await _client.PostAsJsonAsync(_basePath, cliente);
+
+            response.EnsureSuccessStatusCode();
+
+            return response.IsSuccessStatusCode;
+
+        }
+        public async Task<bool> EliminarCliente(int id)
+        {
+            try
+            {
+
+                HttpRequestMessage request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Delete,
+                    Content = JsonContent.Create(id),
+                    RequestUri = new Uri(_basePath, UriKind.Absolute)
+                };
+
+                HttpResponseMessage response = await _client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+    }
+}
---
+++ Clase_16/HttpCliente/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace HttpCliente
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            string url = @"https://localhost:7074/api/Cliente";
+
+            ConsumoApi clienteHttp = new ConsumoApi(url);
+
+            List<Usuario> usuarios = await clienteHttp.FiltrarAsync();
+
+            foreach (Usuario usuario in usuarios)
+            {
+                Console.WriteLine($"Cliente: {usuario.Nombre}");
+            }
+
+            Console.WriteLine("---------- Modificar ----------");
+
+            Usuario usuario1 = new Usuario();
+            usuario1.id = 12;
+            usuario1.Nombre = "Diego";
+            usuario1.Apellido = "Lopez";
+            usuario1.Domicilio = "Camino Cintura 2500";
+            usuario1.Telefono = "123123123132";
+
+            // bool respuesta = await clienteHttp.ModificarCliente(cliente1);
+
+            //Console.WriteLine(respuesta);
+
+            Console.WriteLine("---------- Crear Cliente ----------");
+
+            Usuario usuariosNew = new Usuario();
+
+            usuariosNew.Nombre = "Lucho";
+            usuariosNew.Apellido = "Lopez";
+            usuariosNew.Domicilio = "Camino Cintura 22500";
+            usuariosNew.Telefono = "+123123123132";
+
+            bool respuesta = await clienteHttp.AltaCliente(usuariosNew);
+
+            Console.WriteLine(respuesta);
+
+            Console.WriteLine("---------- Eliminar Cliente ----------");
+
+            respuesta = await clienteHttp.EliminarCliente(13);
+
+            Console.WriteLine(respuesta);
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
